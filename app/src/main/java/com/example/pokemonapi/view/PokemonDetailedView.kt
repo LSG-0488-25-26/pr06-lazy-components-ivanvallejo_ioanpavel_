8d4a243c6923df98c2a98b63ef3dc85e24922317
pavell <pavellupan229@gmail.com>
2026-01-14 13:26:23 +0100

    fix: minor changes
    imports that did not serve any pupose

--- app/src/main/java/com/example/pokemonapi/view/PokemonDetailedView.kt
+++ app/src/main/java/com/example/pokemonapi/view/PokemonDetailedView.kt
@@ -3,7 +3,6 @@ package com.example.pokemonapi.view
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
@@ -11,10 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
